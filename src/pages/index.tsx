c0eaccec2751ef2dbc20dd8fef8f9cf4e83b916c
Bahar Jafarimehr <beatricemikaelson23@gmail.com>
2024-08-01 23:48:21 +0330

    style : implement home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,17 @@
+import Brands from "@/components/home/Brands";
+import Categories from "@/components/home/Categories";
+import DiscountProduct from "@/components/home/swipers/DiscountProduct";
 import ProductsBanner from "@/components/home/swipers/ProductsBanner";
 import { MainLayout } from "@/components/layouts/main/Layout";
 import { ReactElement } from "react";
 
 export default function HomePage() {
   return (
-    <div className="min-h-screen w-full  flex flex-col gap-6  py-12 px-24">
+    <div className="min-h-screen w-full  flex flex-col gap-24  py-12 pb-32 px-24">
       <ProductsBanner />
+      <DiscountProduct/>
+      <Categories/>
+      <Brands/>
     </div>
   );
 }
